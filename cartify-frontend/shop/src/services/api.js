dbcc70579300a1545e7574976f445f2c8c6f4eca
KaminSirisu <Kaminsirisu@gmail.com>
2026-04-22 23:28:05 +0700

    update for deploy front

--- cartify-frontend/shop/src/services/api.js
+++ cartify-frontend/shop/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://project230-cartify.onrender.com'
 
 const api = axios.create({
   baseURL: BASE_URL,
