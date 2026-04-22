5d8a9c737184c21476a69546246f795af8ddf85e
KaminSirisu <Kaminsirisu@gmail.com>
2026-04-22 22:53:37 +0700

    update for deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,6 +34,6 @@ app.use(require('./middleware/errorHandler'));
 
 //Start
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`🚀  Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {console.log('Server running on port', PORT);});
 
 module.exports = app;
